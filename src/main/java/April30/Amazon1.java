28d8ae3550bdef304ca95f92b78fdd101f84ec04
Gowtham <gwtmps@gmail.com>
2024-04-30 17:45:54 +0530

    Added Explicit wait

--- src/main/java/April30/Amazon1.java
+++ src/main/java/April30/Amazon1.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
@@ -14,8 +16,9 @@ import java.util.List;
 public class Amazon1 {
     public static void main(String[] args) throws InterruptedException, StaleElementReferenceException {
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        //driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        WebDriverWait mywait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         String url = "https://www.amazon.in/";
         driver.get(url);
 
@@ -31,6 +34,9 @@ public class Amazon1 {
                 break;
             }
         }
+        WebElement t =options.getFirstSelectedOption();
+        System.out.println(t.getText());
+
         driver.findElement(By.id("nav-search-submit-button")).click();
         List<WebElement> list = driver.findElements(By.xpath("//span[@dir='auto'][contains(.,'Accessories')]"));
         for (WebElement l : list) {
@@ -40,6 +46,7 @@ public class Amazon1 {
             }
         }
         act.moveToElement(driver.findElement(By.xpath("//*[@id=\"a-page\"]/div[1]/div[2]/div[3]/div[1]/div/div/div/ul/li[1]"))).build().perform();
+        mywait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class=\"a-size-base a-color-base\"][contains(.,'Mobile Phone Accessories')]")));
         driver.findElement(By.xpath("//span[@class=\"a-size-base a-color-base\"][contains(.,'Mobile Phone Accessories')]")).click();
         Thread.sleep(5000);
         driver.quit();
