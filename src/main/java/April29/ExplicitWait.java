695dbbd63fc2460790980506bc79e2cc1294048d
Gowtham <gwtmps@gmail.com>
2024-04-30 14:29:25 +0530

    Frames with slider

--- src/main/java/April29/ExplicitWait.java
+++ src/main/java/April29/ExplicitWait.java
@@ -16,7 +16,14 @@ public class ExplicitWait {
         ChromeDriver driver = new ChromeDriver();
        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
        // WebDriverWait wait  = new WebDriverWait(driver, Duration.ofSeconds(180));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        long start = System.currentTimeMillis();
+
         driver.get("https://www.amazon.in/");
+        long finish = System.currentTimeMillis();
+        long totalTime = finish - start;
+        System.out.println("Total Time for page load - "+totalTime);
+
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("shampoo");
         driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
         List<WebElement> d = driver.findElements(By.xpath("//a[@class='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']"));
---
+++ src/main/java/April30/Slider.java
@@ -0,0 +1,38 @@
+package April30;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
+public class Slider {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+       // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://demo.automationtesting.in/Slider.html";
+        String url1 = "https://www.globalsqa.com/demo-site/sliders/#Color%20Picker";
+        driver.get(url1);
+        driver.manage().window().maximize();
+        WebElement fram = driver.findElement(By.xpath("//*[@id=\"post-2673\"]/div[2]/div/div/div[1]/p/iframe"));
+        driver.switchTo().frame(fram);
+        WebElement cursor = driver.findElement(By.xpath("//div[@id='green']/span"));
+        Actions act = new Actions(driver);
+        act.dragAndDropBy(cursor,100,0).build().perform();
+        Thread.sleep(5000);
+        driver.quit();
+
+        //without using Frames
+      /*  WebElement slide = driver.findElement(By.xpath("//div[@id='slider']/a"));
+        Actions act = new Actions(driver);
+        act.dragAndDropBy(slide,300,0).build().perform();
+        Thread.sleep(5000);
+        driver.quit();*/
+
+        //Using Frames
+
+
+    }
+}
